2121596b09f744a6b59b2386d17e44b5fc6e8ae2
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-18 13:30:57 +0900

    Creating layers for the model

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ from tensorflow.keras.utils import to_categorical
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train / 255
 x_test = x_test / 255
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential([
+    Flatten(input_shape=(32, 32, 3)),
+    Dense(1000, activation="relu"),
+    Dense(10, activation="softmax")
+])
