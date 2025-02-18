eb56e5ac4aff13f14b46af0adc27a7ecfc55321e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-18 14:19:36 +0900

    test data changed to validation data

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.utils import to_categorical
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_val, y_val) = cifar10.load_data()
 x_train = x_train / 255
-x_test = x_test / 255
+x_val = x_val / 255
 
 y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+y_val = to_categorical(y_val, 10)
 
 model = Sequential([
     Flatten(input_shape=(32, 32, 3)),
