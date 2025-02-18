8b99a2b36c183c4e441d78cdf5c1957e83fb5268
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-18 14:30:34 +0900

    Code used to train the model is not needed anymore

--- main.py
+++ main.py
@@ -3,28 +3,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import streamlit as st 
-
 from PIL import Image
-
 import tensorflow as tf 
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense
-from tensorflow.keras.utils import to_categorical
 
-(x_train, y_train), (x_val, y_val) = cifar10.load_data()
-x_train = x_train / 255
-x_val = x_val / 255
-
-y_train = to_categorical(y_train, 10)
-y_val = to_categorical(y_val, 10)
-
-model = Sequential([
-    Flatten(input_shape=(32, 32, 3)),
-    Dense(1000, activation="relu"),
-    Dense(10, activation="softmax")
-])
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_val, y_val))
-model.save("cifar10_model.keras")
