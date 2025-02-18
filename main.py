681a332f74dab5a38353fa5662c4e17f66ccc2f3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-18 13:26:56 +0900

    We get values from 0 to 1 when dividing by 255

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # Web Application in Pure Python
 # Using the Package Streamlit to build a UI
+import numpy as np 
+import matplotlib.pyplot as plt 
+import streamlit as st 
+
+from PIL import Image
+
+import tensorflow as tf 
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train / 255
+x_test = x_test / 255
