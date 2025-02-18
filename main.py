663f103adff72ab9b57514f9706b89cb6b319a3a
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-18 14:39:21 +0900

    Uploading an image so we can predict it with the trained model

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ import streamlit as st
 from PIL import Image
 import tensorflow as tf 
 
+def main():
+    st.title("Cifar10-Model Web Classifier")
+    st.write("Upload any image that you think fits into one of the classes. Let's see if the prediction is correct!")
+    file = st.file_uploader("Please upload an image", type=["jpg", "png"])
+
+    if file:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+    else:
+        st.text("You have not uploaded an image yet.")
